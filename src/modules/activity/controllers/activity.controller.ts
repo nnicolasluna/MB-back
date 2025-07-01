edb612a415d123f85943e97dd660f75fde552d4b
Joel Nicolas Luna <nnicolasluna@gmail.com>
2025-07-01 16:58:33 -0400

    integracion de lista de participantes

--- src/modules/activity/controllers/activity.controller.ts
+++ src/modules/activity/controllers/activity.controller.ts
@@ -9,7 +9,7 @@ import * as fs from 'fs';
 import { SetMetadata } from '@nestjs/common';
 @Controller('activity')
 export class ActivityController {
-	constructor(private readonly activityService: ActivityService) { }
+	constructor(private readonly activityService: ActivityService) {}
 
 	@Post()
 	create(@Body() createActivityDto: any) {
@@ -101,5 +101,23 @@ export class ActivityController {
 			message: 'Archivo subido correctamente',
 			filename: file.filename,
 		};
+	}
+	@Get('download-list/:filename')
+	@SetMetadata('isPublic', true)
+	async downloadFileList(@Param('filename') filename: string, @Res() res: Response) {
+		const filePath = path.join('./uploads/TareaByFecha', filename);
+
+		// Verificar si el archivo existe
+		if (!fs.existsSync(filePath)) {
+			return res.status(404).json({ message: 'Archivo no encontrado' });
+		}
+
+		// Servir el archivo para descarga
+		res.download(filePath, filename, (err) => {
+			if (err) {
+				console.error('Error al descargar archivo:', err);
+				res.status(500).json({ message: 'Error al descargar el archivo' });
+			}
+		});
 	}
 }
